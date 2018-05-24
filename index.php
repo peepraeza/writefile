03e67b9cf39cb93674218b16292df6004da218d2
Peerawit <peerawit.sc@gmail.com>
2018-05-24 22:34:14 +0700

    test db

--- index.php
+++ index.php
@@ -1,8 +1,36 @@
 <html><body>
 <?php
+
+$servername = "us-cdbr-iron-east-04.cleardb.net";
+$username = "baf563204f6a55";
+$password = "2e0a5a96";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
+
+
+$sql = "SELECT * FROM user";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "user_id: " . $row["uid"]. " - Name: " . $row["name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 
 echo 'Click to write file!';
 
